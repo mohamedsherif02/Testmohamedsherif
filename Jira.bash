23c2ffcfa8c8dbdcbe626ce965e76bcb20f522ae
mohamedsherif02 <mohamedcherif03@gmail.com>
2024-07-30 13:06:47 +0300

    Update Jira.bash

--- Jira.bash
+++ Jira.bash
@@ -1,32 +1,27 @@
-@echo off
-setlocal
+#!/bin/bash
 
-REM Set your Jira credentials and endpoint
-set "JIRA_USER=ahmeds.elnozahy@gmail.com"
-set "JIRA_API_TOKEN=ATATT3xFfGF0hAyV6gXAlbYVNZfU5drSW-nUtEeitDa1SEg4frpd50-LOr48uJIUGCgtyRlgah_T740q_kbj45x6HLRuBR6FPnleO_tZQ3f1jNITL6myMBJUyQIjDHa_TLFvHkfdtR2Ui88NV_GrENwq-BWwspdzk8yLvRCbLSc2OjQwluMR_9U=319AB789"
-set "JIRA_URL=https://ahmedselnozahy.atlassian.net/rest/api/2/search?jql=project=CCB"
+# Set your Jira credentials and endpoint
+JIRA_USER="mohamedcherif03@gmail.com"
+JIRA_API_TOKEN="ATATT3xFfGF0gOrH703HS7ywca3xlxxt3ZsgZjdhhpLlhskh9Uug2ft39GVD6lpaBE05YWlOAZqtw9P0TCTrWpcOUlK7vc0uE4u8bqPedsYhUV8BLwyjL6myl2feo0gZ2sRldsjConk7kl-bgKu7BkqD7XyMRqFa8ILFtPmvUtzIOLwClesxlWg=577DF795"
+JIRA_URL="https://ahmedselnozahy.atlassian.net/rest/api/2/search?jql=project=CCB"
 
-REM Output file
-set "OUTPUT_FILE=jira_timeline.csv"
+# Output file
+OUTPUT_FILE="jira_all.csv"
 
-REM Make API request and save the response to a file
-curl -u %JIRA_USER%:%JIRA_API_TOKEN% -X GET -H "Content-Type: application/json" %JIRA_URL% -o response.json
+# Make API request and save the response to a file
+curl -u $JIRA_USER:$JIRA_API_TOKEN -X GET -H "Content-Type: application/json" $JIRA_URL -o response.json
 
-REM Check if the response contains an error
-jq -e .errors response.json >nul 2>&1
-if %ERRORLEVEL% equ 0 (
-    echo Error in response
-    type response.json
-    exit /b 1
-)
+# Check if the response contains an error
+if jq -e .errors response.json > /dev/null 2>&1; then
+    echo "Error in response"
+    cat response.json
+    exit 1
+fi
 
-REM Process the JSON response to extract the data and save it as CSV
-jq -r ".issues[] | [.key, .fields.summary, .fields.assignee.displayName, .fields.status.name, .fields.created, .fields.updated] | @csv" response.json > %OUTPUT_FILE%
+# Process the JSON response to extract the data and save it as CSV
+jq -r '.issues[] | [.key, .fields.summary, .fields.assignee.displayName, .fields.status.name, (.fields.customfield_10020[0] | .id // "No Sprint"), .fields.created, .fields.updated] | @csv' response.json > $OUTPUT_FILE
 
-REM Clean up
-del response.json
+# Clean up
+rm response.json
 
-echo CSV file has been saved to %OUTPUT_FILE%
-
-endlocal
-pause
+echo "CSV file has been saved to $OUTPUT_FILE"
